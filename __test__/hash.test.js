a697451c8b8d23b18c724da21b672841003dd7d9
Ujjwal Sharma <usharma1998@gmail.com>
2019-01-17 02:41:24 +0530

    test: add basic tests for hash.hash

---
+++ __test__/hash.test.js
@@ -0,0 +1,13 @@
+const hash = require('../hash');
+
+const hashOutput =
+  'dffd6021bb2bd5b0af676290809ec3a53191dd81c7f70a4b28688a362182986f';
+
+test('hashes strings', () => {
+  expect(hash.hash('sha256', 'Hello, World!').toString('hex')).toBe(hashOutput);
+});
+
+test('hashes buffers', () => {
+  const buf = Buffer.from('Hello, World!');
+  expect(hash.hash('sha256', buf).toString('hex')).toBe(hashOutput);
+});
